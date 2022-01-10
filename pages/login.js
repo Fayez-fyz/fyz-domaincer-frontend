f8a2b30202d382c0c28a2b273acb37a700235c6e
Fayez-fyz <zeyaffayez007@gmail.com>
2022-01-10 17:54:12 +0530

    login-issue

--- pages/login.js
+++ pages/login.js
@@ -35,7 +35,7 @@ const Login = () => {
         });
         //save in local storage
         window.localStorage.setItem("auth",JSON.stringify(data)) 
-        router.push(`/${state && state.token && state.user.role === 'Recuirter' ? 'admin' : 'user'}/dashboard`)
+        router.push(`/${state && state.user.role == 'Recuirter' ? 'admin' : 'user'}/dashboard`)
 
         
       }
@@ -44,10 +44,14 @@ const Login = () => {
       setloading(false);
     }
   };
-  // if (state && state.user.role == 'Recuirter') router.push("/admin/dashboard")
-  
+  if (state && state.user.role == 'Recuirter') {
+    router.push("/admin/dashboard")
+  }else if (state && state.user.role == 'Candidate') {
+    router.push("/user/dashboard")
+  }
+      
 
-  // if (state && state.user.role == 'Candidate') router.push("/user/dashboard");
+ 
   
   return (
     <div className="container-fluid">
